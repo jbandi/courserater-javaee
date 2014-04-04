c378c9dbd4c16bbefbee0220850a4a0d9fc91d47
Jonas Bandi <dev (at) jonasbandi (dot) net>
2014-04-05 01:31:51 +0200

    Fixing CORS filter

--- src/main/java/net/jonasbandi/courserater/rest/CorsFilter.java
+++ src/main/java/net/jonasbandi/courserater/rest/CorsFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebFilter(filterName = "CorsFilter", urlPatterns = {"/*"})
@@ -19,14 +20,23 @@ public class CorsFilter implements Filter{
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        final HttpServletResponse httpResponse = (HttpServletResponse)servletResponse;
+        final HttpServletRequest httpRequest = (HttpServletRequest)servletRequest;
+    	final HttpServletResponse httpResponse = (HttpServletResponse)servletResponse;
 
+    	// For most requests the headers have to be added before further processing, afterwards they just don't get added
         httpResponse.addHeader("Access-Control-Allow-Origin", "*");
         httpResponse.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         httpResponse.addHeader("Access-Control-Allow-Headers", "x-requested-with, accept, origin, authorization");
-        httpResponse.addHeader("Access-Control-Max-Age", "1728000");
+        httpResponse.addHeader("Access-Control-Max-Age", "10");
         
         filterChain.doFilter(servletRequest,servletResponse);
+        
+        // For OPTIONS requests the headers have to be added after the other processing
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpResponse.addHeader("Access-Control-Allow-Headers", "x-requested-with, accept, origin, authorization");
+        httpResponse.addHeader("Access-Control-Max-Age", "10");
+        
     }
 
     @Override
