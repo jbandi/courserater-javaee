990cb831b0aaa88d858cb57f31e317f25baa5ac8
Jonas Bandi <dev (at) jonasbandi (dot) net>
2014-04-05 21:50:11 +0200

    Changes for IntelliJ integration.

--- src/main/java/net/jonasbandi/courserater/rest/CorsFilter.java
+++ src/main/java/net/jonasbandi/courserater/rest/CorsFilter.java
@@ -1,6 +1,7 @@
 package net.jonasbandi.courserater.rest;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -14,6 +15,9 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebFilter(filterName = "CorsFilter", urlPatterns = {"/*"})
 public class CorsFilter implements Filter{
+	
+	private final static Logger log = Logger.getLogger(CorsFilter.class.getName()); 
+	
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
@@ -22,11 +26,16 @@ public class CorsFilter implements Filter{
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         final HttpServletRequest httpRequest = (HttpServletRequest)servletRequest;
     	final HttpServletResponse httpResponse = (HttpServletResponse)servletResponse;
+    	
+    	// Java EE 7 adds javax.ws.rs.container.ContainerResponseFilter ... maybe a better solution?
+    	
+    	String method = httpRequest.getMethod();
+    	log.info("Http request with method: " + method);
 
     	// For most requests the headers have to be added before further processing, afterwards they just don't get added
         httpResponse.addHeader("Access-Control-Allow-Origin", "*");
         httpResponse.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        httpResponse.addHeader("Access-Control-Allow-Headers", "x-requested-with, accept, origin, authorization");
+        httpResponse.addHeader("Access-Control-Allow-Headers", "x-requested-with, accept, origin, authorization, X-PINGOTHER");
         httpResponse.addHeader("Access-Control-Max-Age", "10");
         
         filterChain.doFilter(servletRequest,servletResponse);
@@ -34,7 +43,7 @@ public class CorsFilter implements Filter{
         // For OPTIONS requests the headers have to be added after the other processing
         httpResponse.setHeader("Access-Control-Allow-Origin", "*");
         httpResponse.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        httpResponse.addHeader("Access-Control-Allow-Headers", "x-requested-with, accept, origin, authorization");
+        httpResponse.addHeader("Access-Control-Allow-Headers", "x-requested-with, accept, origin, authorization, X-PINGOTHER");
         httpResponse.addHeader("Access-Control-Max-Age", "10");
         
     }
